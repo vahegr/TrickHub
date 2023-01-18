5b66361dd9a44549e2565566d45ae71c3188a7b1
Vahe Grigorian <vahegrigorian447@gmail.com>
2023-01-19 03:21:30 +0330

    article serializer created

---
+++ article_app/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = (
+            'parent', 'slug', 'user',
+            'title', 'description', 'image',
+            'created_at', 'jalali_created', 'hits')
