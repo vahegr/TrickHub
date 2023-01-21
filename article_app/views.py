8030aab6a984737fc57521f93505310fc00d554b
Vahe Grigorian <vahegrigorian447@gmail.com>
2023-01-22 03:04:02 +0330

    article update and delete view changed

--- article_app/views.py
+++ article_app/views.py
@@ -29,6 +29,43 @@ class ArticlesView(APIView):
 
 class ArticleDetailView(APIView):
     def get(self, request, id, slug):
-        article = Article.objects.get(id=id, slug=slug)
+        article = Article.objects.get(id=id, slug=slug, allowing=True)
         ser = ArticleSerializer(instance=article)
         return Response(ser.data)
+
+
+class CreateArticleView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            if request.user.is_authenticated:
+                serializer.validated_data['user'] = request.user
+            serializer.save()
+            return Response({"response": "created"}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateArticleView(APIView):
+    permission_classes = [IsAdminUser, IsAuthenticated, OwnerOrRead]
+
+    def put(self, request, id):
+        instance = Article.objects.get(id=id)
+        self.check_object_permissions(request, instance)
+        serializer = ArticleSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.update(instance=instance, validated_data=serializer.validated_data)
+            return Response({"response": "updated"}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteArticleView(APIView):
+    permission_classes = [IsAdminUser, IsAuthenticated, OwnerOrRead]
+
+    def delete(self, request, id):
+        instance = Article.objects.get(id=id)
+        instance.delete()
+        return Response({"response": "deleted"}, status=status.HTTP_200_OK)
