473e60e5f332f2bea3ac63b32b5cef72295473f7
Vahe Grigorian <vahegrigorian447@gmail.com>
2023-01-22 04:39:33 +0330

    comment serializer created

--- article_app/serializers.py
+++ article_app/serializers.py
@@ -1,18 +1,32 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment, Like
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
+    user = serializers.SlugRelatedField(slug_field='username', read_only=True)
+
     class Meta:
         model = Article
         fields = (
             'id', 'parent', 'slug', 'user',
             'title', 'description', 'image',
-            'created_at', 'jalali_created', 'hits')
+            'created_at', 'jalali_created', 'hits', 'comments')
 
-        def get_image(self, obj):               # if view was ApiView this is necessary
-            request = self.context.get('request')
-            if obj.image:
-                image_url = obj.image.url
-                return request.build_absolute_uri(image_url)
-            return None
+    def get_image(self, obj):               # if view was ApiView this is necessary
+        request = self.context.get('request')
+        if obj.image:
+            image_url = obj.image.url
+            return request.build_absolute_uri(image_url)
+        return None
+
+    def get_comments(self, obj):
+        serializer = CommentSerializer(instance=obj.comments.all(), many=True)
+        return serializer.data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'video', 'user', 'parent', 'comment', 'created_time', 'jalali_created')
