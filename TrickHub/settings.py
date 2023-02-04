3ab414688f2743eace0e7d092526388ed42dce48
vahe grigorian <vahegrigorian447@gmail.com>
2023-02-04 16:11:40 +0330

    static_root address added

--- TrickHub/settings.py
+++ TrickHub/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
