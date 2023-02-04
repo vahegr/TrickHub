d33d7ceda34d2ec2086ed32f4795bbf9ddc23939
vahe grigorian <vahegrigorian447@gmail.com>
2023-02-04 12:53:07 +0330

    user register system created

--- account_app/serializers.py
+++ account_app/serializers.py
@@ -3,6 +3,22 @@ from .models import User
 from django.utils import timezone
 
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'full_name')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- account_app/views.py
+++ account_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from account_app.permissions import OwnerOrRead
+from .serializers import UserSerializer, RegisterSerializer
+from .models import User
 
-# Create your views here.
+
+class Register(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User Created Successfully.  Now perform Login to get your token",
+        })
