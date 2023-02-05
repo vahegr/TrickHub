f58787e47c4b21ed5087cb8cf1946d9cd35398bb
vahegr <vahegrigorian447@gmail.com>
2023-02-06 00:21:00 +0330

    verbos names deleted from user model fields

--- account_app/models.py
+++ account_app/models.py
@@ -5,80 +5,64 @@ from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
     email = models.EmailField(
-        verbose_name='آدرس ایمیل',
         max_length=255,
         unique=True,
     )
     username = models.CharField(
         max_length=50,
         unique=True,
-        verbose_name='نام کاربری'
     )
     full_name = models.CharField(
         max_length=100,
         null=True,
         blank=True,
-        verbose_name='نام و نام خانوادگی'
     )
     phone = models.CharField(
         max_length=12,
         unique=True,
         blank=True,
         null=True,
-        verbose_name='شماره مبایل'
     )
     image = models.ImageField(
         upload_to='images/users',
         null=True,
         blank=True,
-        verbose_name='تصویر کاربر'
     )
     bio = models.TextField(
         null=True,
         blank=True,
-        verbose_name='درباره'
     )
     instagram = models.URLField(
         blank=True,
         null=True,
         unique=True,
-        verbose_name='آیدی اینستاگرام'
     )
     github = models.URLField(
         blank=True,
         null=True,
         unique=True,
-        verbose_name='آدرس گیتهاب'
     )
     linkedin = models.URLField(
         blank=True,
         null=True,
         unique=True,
-        verbose_name='آدرس لینکدین'
     )
     twitter = models.URLField(
         blank=True,
         null=True,
         unique=True,
-        verbose_name='آدرس توییتر'
     )
     is_active = models.BooleanField(
         default=True,
-        verbose_name='فعالیت'
     )
     is_admin = models.BooleanField(
         default=False,
-        verbose_name='ادمین'
     )
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', ]
-
-    class Meta:
-        verbose_name = 'کاربر'
-        verbose_name_plural = 'کاربر ها'
 
     def __str__(self):
         return self.username
